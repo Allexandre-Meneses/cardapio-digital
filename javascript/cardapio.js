66403fd8e6c48312274d994cee336dbe1fe97f97
Allexandre-Meneses <meloxandy@hotmail.com>
2022-04-04 14:31:06 -0300

    Criando funcionalidade do botao Adicionar

--- javascript/cardapio.js
+++ javascript/cardapio.js
@@ -15,5 +15,17 @@ function abreProduto(produto_id) {
 }
 
 function AdicionarPedido() {
-    
+    let produto_id = localStorage.getItem('produto_id')
+    let pedidos = JSON.parse(localStorage.getItem('pedidos'))
+
+    if (pedidos == null) {
+        localStorage.setItem('pedidos', JSON.stringify(pedidos = []))
+    }
+
+    pedidos.push(Number(produto_id))
+
+    localStorage.setItem('pedidos', JSON.stringify(pedidos))
+
+
+
 }
